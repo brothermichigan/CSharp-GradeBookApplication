3d7eb2fd3d3fa24cdb1afe4c676a04a4c926d05f
brothermichigan <morris.aaronm@gmail.com>
2018-12-12 13:13:26 -0500

    Add GetLetterGrade for Ranked GradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,12 +1,49 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
     class RankedGradeBook : BaseGradeBook
     {
+        private List<double> averageGrades = new List<double>();
+
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            foreach (var student in Students)
+            {
+                var grade = student.AverageGrade;
+                averageGrades.Add(grade);
+            }
+            averageGrades = averageGrades.OrderByDescending(d => d).ToList();
+
+            var gradeBinSize = Students.Count / 5;
+            var gradeBin = averageGrades.IndexOf(averageGrade);
+
+            switch (gradeBin / gradeBinSize)
+            {
+                case 0:
+                    return 'A';
+                case 1:
+                    return 'B';
+                case 2:
+                    return 'C';
+                case 3:
+                    return 'D';
+            }
+
+            return 'F';
         }
     }
 }
